6afe157ee5f01c8e5966365c9aa8a60eb39ff33e
NipunK <nipyantha@gmail.com>
2025-06-10 06:19:04 +0530

    fix: pipeline github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  site: "https://sanzserif.github.io",
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),

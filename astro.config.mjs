bcb4ed3193289c5d3186e8015eb180310e29cc23
NipunK <nipyantha@gmail.com>
2025-06-10 06:44:13 +0530

    refactor: build config changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://sanzserif.github.io",
-  integrations: [tailwind()],
-  adapter: vercel(),
-  output: "server",
+  base: "/whiskrs-digital-portfolio", // Replace with your actual repo name
   integrations: [tailwind()],
 });

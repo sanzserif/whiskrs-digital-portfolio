45c327e774bf322309156cc220540e39979018da
NipunK <nipyantha@gmail.com>
2025-06-10 07:11:13 +0530

    refactor: removed blog component and cleared ss rendering config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+// Import the Vercel adapter
+// import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sanzserif.github.io",
-  base: "/whiskrs-digital-portfolio", // Replace with your actual repo name
   integrations: [tailwind()],
+  // output: "server",
+  // adapter: vercel(),
 });
